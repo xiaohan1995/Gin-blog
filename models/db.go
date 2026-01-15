663fde9d8e7eb8b0e213560ccfa7c1c31004f26c
Shawn Hans <63792674+xiaohan1995@users.noreply.github.com>
2026-01-15 16:02:10 +0800

    change pass

--- models/db.go
+++ models/db.go
@@ -32,7 +32,7 @@ type Comment struct {
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "root:parents123@tcp(127.0.0.1:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:password123@tcp(127.0.0.1:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
